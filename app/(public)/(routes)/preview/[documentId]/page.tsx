47c918b862fc697cebcb74e5754ae28351ab4923
Anmol Gangwar <anmolgangwar64@gmail.com>
2023-11-09 02:50:59 +0530

    fix :bug: fixing preview page bgcolor mismatch

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -53,9 +53,9 @@ const PreviewDocumentPage = ({ params }: DocumentIdPageProps) => {
   }
 
   return (
-    <div className="pb-40">
+    <div className="pb-40 min-h-full dark:bg-[#1F1F1F]  ">
       <CoverImage preview url={document.coverImage} />
-      <div className="md:max-w-3xl lg:max-w-4xl mx-auto ">
+      <div className="md:max-w-3xl  lg:max-w-4xl mx-auto ">
         <Toolbar preview initialData={document} />
         <Editor
           onChange={onChange}
